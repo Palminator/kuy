e35fd9777ff2881ad99eb7df70b2eb9464f2255a
parameth <palm24572@gmail.com>
2019-11-07 00:14:02 +0700

    erfer

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.textView2);
+        text.setPaintFlags(text.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+    }
+}
